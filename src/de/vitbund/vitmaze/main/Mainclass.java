a9e3ee2f6100285b5be68f3a64a13f2140c4c962
fritzkoe <fritzkoegame@gmail.com>
2020-07-19 16:29:05 +0200

    Dokumentaion

--- src/de/vitbund/vitmaze/main/Mainclass.java
+++ src/de/vitbund/vitmaze/main/Mainclass.java
@@ -2,9 +2,14 @@ package de.vitbund.vitmaze.main;
 
 
 import de.vitbund.vitmaze.eingabe.Eingabe;
-
+/**
+ * instanziert das Spiel
+ * @author Arbeitstitel
+ * @version 1.0
+ *
+ */
 public class Mainclass {
-	
+	//Mainmethode
 	public static void main(String[] args) {
 		
 		Spiel neuesSpiel = new Spiel();
